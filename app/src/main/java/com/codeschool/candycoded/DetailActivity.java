fb30aee71ea6739b2a17745117491ecc8544a260
Yury Kozhenov <yurykozhenov@gmail.com>
2017-11-22 00:58:54 +0200

    Add offline support with caching data in SQLite

--- app/src/main/java/com/codeschool/candycoded/DetailActivity.java
+++ app/src/main/java/com/codeschool/candycoded/DetailActivity.java
@@ -1,11 +1,14 @@
 package com.codeschool.candycoded;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.codeschool.candycoded.CandyContract.CandyEntry;
 import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
@@ -17,36 +20,30 @@ public class DetailActivity extends AppCompatActivity {
 
         Intent intent = DetailActivity.this.getIntent();
 
-        String candyName = "";
-        if (intent.hasExtra("candy_name")) {
-            candyName = intent.getStringExtra("candy_name");
+        if (intent.hasExtra("position")) {
+            int position = intent.getIntExtra("position", 0);
+
+            CandyDbHelper candyDbHelper = new CandyDbHelper(this);
+            SQLiteDatabase db = candyDbHelper.getWritableDatabase();
+            Cursor cursor = db.rawQuery("SELECT * FROM candy", null);
+            cursor.moveToPosition(position);
+
+            String candyName = cursor.getString(cursor.getColumnIndexOrThrow(CandyEntry.COLUMN_NAME_NAME));
+            String candyPrice = cursor.getString(cursor.getColumnIndexOrThrow(CandyEntry.COLUMN_NAME_PRICE));
+            String candyImage = cursor.getString(cursor.getColumnIndexOrThrow(CandyEntry.COLUMN_NAME_IMAGE));
+            String candyDesc = cursor.getString(cursor.getColumnIndexOrThrow(CandyEntry.COLUMN_NAME_DESC));
+
+            ImageView imageView = this.findViewById(R.id.image_view_candy);
+            Picasso.with(this).load(candyImage).into(imageView);
+
+            TextView textViewName = this.findViewById(R.id.text_view_name);
+            textViewName.setText(candyName);
+
+            TextView textViewPrice = this.findViewById(R.id.text_view_price);
+            textViewPrice.setText("$" + candyPrice + "/lb");
+
+            TextView textViewDesc = this.findViewById(R.id.text_view_desc);
+            textViewDesc.setText(candyDesc);
         }
-
-        String candyImage = "";
-        if (intent.hasExtra("candy_image")) {
-            candyImage = intent.getStringExtra("candy_image");
-        }
-
-        String candyPrice = "";
-        if (intent.hasExtra("candy_price")) {
-            candyPrice = intent.getStringExtra("candy_price");
-        }
-
-        String candyDesc = "";
-        if (intent.hasExtra("candy_desc")) {
-            candyDesc = intent.getStringExtra("candy_desc");
-        }
-
-        ImageView imageView = this.findViewById(R.id.image_view_candy);
-        Picasso.with(this).load(candyImage).into(imageView);
-
-        TextView textViewName = this.findViewById(R.id.text_view_name);
-        textViewName.setText(candyName);
-
-        TextView textViewPrice = this.findViewById(R.id.text_view_price);
-        textViewPrice.setText("$" + candyPrice + "/lb");
-
-        TextView textViewDesc = this.findViewById(R.id.text_view_desc);
-        textViewDesc.setText(candyDesc);
     }
 }
--- app/src/main/java/com/codeschool/candycoded/MainActivity.java
+++ app/src/main/java/com/codeschool/candycoded/MainActivity.java
@@ -1,41 +1,39 @@
 package com.codeschool.candycoded;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.codeschool.candycoded.CandyContract.CandyEntry;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.TextHttpResponseHandler;
 
-import java.util.ArrayList;
-
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
     private Candy[] candies;
+    private CandyDbHelper candyDbHelper = new CandyDbHelper(this);
+    private CandyCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ArrayList<String> candyList = new ArrayList<>();
-
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this,
-                R.layout.list_item_candy,
-                R.id.text_view_candy,
-                candyList
-        );
+        SQLiteDatabase db = candyDbHelper.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM candy", null);
+        adapter = new CandyCursorAdapter(this, cursor);
 
         ListView listView = this.findViewById(R.id.list_view_candy);
         listView.setAdapter(adapter);
@@ -49,11 +47,11 @@ public class MainActivity extends AppCompatActivity {
                 Gson gson = new GsonBuilder().create();
                 candies = gson.fromJson(response, Candy[].class);
 
-                adapter.clear();
+                addCandiesToDatabase(candies);
 
-                for (Candy candy: candies) {
-                    adapter.add(candy.name);
-                }
+                SQLiteDatabase db = candyDbHelper.getWritableDatabase();
+                Cursor cursor = db.rawQuery("SELECT * FROM candy", null);
+                adapter.changeCursor(cursor);
             }
 
             @Override
@@ -77,12 +75,24 @@ public class MainActivity extends AppCompatActivity {
 //                Toast toast = Toast.makeText(MainActivity.this, "" + i, Toast.LENGTH_SHORT);
 //                toast.show();
                 Intent detailIntent = new Intent(MainActivity.this, DetailActivity.class);
-                detailIntent.putExtra("candy_name", candies[i].name);
-                detailIntent.putExtra("candy_image", candies[i].image);
-                detailIntent.putExtra("candy_price", candies[i].price);
-                detailIntent.putExtra("candy_desc", candies[i].description);
+                detailIntent.putExtra("position", i);
                 startActivity(detailIntent);
             }
         });
+    }
+
+    public void addCandiesToDatabase(Candy[] candies) {
+        SQLiteDatabase db = candyDbHelper.getWritableDatabase();
+
+        for (Candy candy : candies) {
+            ContentValues values = new ContentValues();
+
+            values.put(CandyEntry.COLUMN_NAME_NAME, candy.name);
+            values.put(CandyEntry.COLUMN_NAME_PRICE, candy.price);
+            values.put(CandyEntry.COLUMN_NAME_DESC, candy.description);
+            values.put(CandyEntry.COLUMN_NAME_IMAGE, candy.image);
+
+            db.insert(CandyEntry.TABLE_NAME, null, values);
+        }
     }
 }
